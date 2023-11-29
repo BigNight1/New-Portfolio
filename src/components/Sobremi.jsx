c799147a69a04484ae2d4dc2ffd56e5600ed8d6c
Edu Armas <edu_armas11@hotmail.com>
2023-11-28 19:30:11 -0500

    Arreglas varias cosas

--- src/components/Sobremi.jsx
+++ src/components/Sobremi.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import sobre from "../styles/sobremi.module.css"
 
 const skills = [
   {
@@ -45,48 +46,51 @@ const skills = [
 
 const Sobremi = () => {
   return (
-    <div>
-      <div>
-        <h2>Sobre Mi</h2>
-      </div>
-      <div>
-        1 año de experiencia programando paginas web y creando paginas
-        interesantes
-      </div>
-      <div className="descripcion">
-        <div className="title2">Conóceme más</div>
-        <div>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum
-            necessitatibus deserunt corporis at veritatis vitae, quae id ut
-            doloribus adipisci maxime porro perspiciatis amet
-          </p>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum
-            necessitatibus deserunt corporis at veritatis vitae, quae id ut
-            doloribus adipisci maxime porro perspiciatis amet
-          </p>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum
-            necessitatibus deserunt corporis at veritatis vitae, quae id ut
-            doloribus adipisci maxime porro perspiciatis amet
-          </p>
+    <section>
+      <div className="content">
+        <h2 className={sobre.title}>Sobre Mi</h2>
+        <div className={sobre.description}>
+          1 año de experiencia programando paginas web y creando paginas
+          interesantes
         </div>
-        <div className="botonProyectos">
-          <a href="#Proyectos">Proyectos</a>
+        <div className={sobre.content_wrapper}>
+
+        <div className={sobre.descripcion}>
+          <div className="title2">Conóceme más</div>
+          <div>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum
+              necessitatibus deserunt corporis at veritatis vitae, quae id ut
+              doloribus adipisci maxime porro perspiciatis amet
+            </p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum
+              necessitatibus deserunt corporis at veritatis vitae, quae id ut
+              doloribus adipisci maxime porro perspiciatis amet
+            </p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum
+              necessitatibus deserunt corporis at veritatis vitae, quae id ut
+              doloribus adipisci maxime porro perspiciatis amet
+            </p>
+          </div>
+          <div className="botonProyectos">
+            <a href="#Proyectos">Proyectos</a>
+          </div>
+        </div>
+        <div className={sobre.skills}>
+          <div>Mis habilidades y herramientas</div>
+          <div className={sobre.skills_list}>
+            {skills.map((skill) => (
+              <span className={sobre.skill_block} key={skill.id}>
+                {skill.name}
+              </span>
+            ))}
+          </div>
         </div>
       </div>
-      <div className="skills">
-        <div>Mis habilidades y herramientas</div>
-        <div className="skills-list">
-          {skills.map((skill) => (
-            <span className="skill-block" key={skill.id}>
-              {skill.name}
-            </span>
-          ))}
-        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
