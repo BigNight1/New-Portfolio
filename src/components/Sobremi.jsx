bce80542d5688f2a0a2d11afa9be8e6227e90255
Edu Walter Armas Aliaga <edu_armas11@hotmail.com>
2023-11-30 11:06:15 -0500

    seguimosconavances

--- src/components/Sobremi.jsx
+++ src/components/Sobremi.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import sobre from "../styles/sobremi.module.css"
+import sobre from "../styles/sobremi.module.css";
 
 const skills = [
   {
@@ -12,7 +12,7 @@ const skills = [
   },
   {
     id: 3,
-    name: "JavaScript",
+    name: "SASS",
   },
   {
     id: 4,
@@ -20,7 +20,7 @@ const skills = [
   },
   {
     id: 5,
-    name: "SASS",
+    name: "JavaScript",
   },
   {
     id: 6,
@@ -43,52 +43,61 @@ const skills = [
     name: "Git",
   },
 ];
+const proyecto = [
+  {
+    id: 1,
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumnecessitatibus deserunt corporis at veritatis vitae, quae id utdoloribus",
+  },
+  {
+    id: 2,
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumnecessitatibus deserunt corporis at veritatis vitae, quae id utdoloribus",
+  },
+  {
+    id: 3,
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumnecessitatibus deserunt corporis at veritatis vitae, quae id utdoloribus",
+  },
+];
 
 const Sobremi = () => {
   return (
     <section>
-      <div className="content">
+      <div className={sobre.content}>
         <h2 className={sobre.title}>Sobre Mi</h2>
         <div className={sobre.description}>
           1 año de experiencia programando paginas web y creando paginas
           interesantes
         </div>
-        <div className={sobre.content_wrapper}>
 
-        <div className={sobre.descripcion}>
-          <div className="title2">Conóceme más</div>
-          <div>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum
-              necessitatibus deserunt corporis at veritatis vitae, quae id ut
-              doloribus adipisci maxime porro perspiciatis amet
-            </p>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum
-              necessitatibus deserunt corporis at veritatis vitae, quae id ut
-              doloribus adipisci maxime porro perspiciatis amet
-            </p>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum
-              necessitatibus deserunt corporis at veritatis vitae, quae id ut
-              doloribus adipisci maxime porro perspiciatis amet
-            </p>
+        <div className={sobre.content_wrapper}>
+          <div className={sobre.descripcion_two}>
+            <div className={sobre.title2}>Conóceme más</div>
+            <div>
+              {proyecto.map((proyect) => {
+                return (
+                  <p className={sobre.proyectos_pa} key={proyect.id}>
+                    {proyect.description}
+                  </p>
+                );
+              })}
+            </div>
+            <div className="botonProyectos">
+              <a href="#Proyectos">Proyectos</a>
+            </div>
           </div>
-          <div className="botonProyectos">
-            <a href="#Proyectos">Proyectos</a>
-          </div>
-        </div>
-        <div className={sobre.skills}>
-          <div>Mis habilidades y herramientas</div>
-          <div className={sobre.skills_list}>
-            {skills.map((skill) => (
-              <span className={sobre.skill_block} key={skill.id}>
-                {skill.name}
-              </span>
-            ))}
+          <div className={sobre.skills}>
+            <div>Mis habilidades y herramientas</div>
+            <div className={sobre.skills_list}>
+              {skills.map((skill) => (
+                <span className={sobre.skill_block} key={skill.id}>
+                  {skill.name}
+                </span>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
       </div>
     </section>
   );
