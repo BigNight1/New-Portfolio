e58523f8f3a3226761c9a704a425f7e1adabbe8f
Edu Walter Armas Aliaga <edu_armas11@hotmail.com>
2024-01-14 19:43:35 -0500

    Finish

--- src/components/Experience/experience.jsx
+++ src/components/Experience/experience.jsx
@@ -2,37 +2,17 @@ import React from "react";
 import ExperienceItem from "./experienceItem.jsx";
 import styled from "styled-components";
 import { Briefcase } from "../Icons/Briefcase.jsx";
+import { useTranslation } from "react-i18next";
 
 const Experience = () => {
-  const EXPERIENCE = [
-    {
-      id: 1,
-      date: "ENERO 2024",
-      title: "Creador de Contenido - Programación",
-      description:
-        "Genero contenido en el ámbito de la programación, enseñando cómo crear proyectos atractivos y llamativos para atraer a más personas al fascinante mundo de la programación.",
-    },
-    {
-      id: 2,
-      date: "Diciembre 2023",
-      title: "Proyecto Full Stack - Ecommerce - MongoDb",
-      description:
-        "Lideré el desarrollo del lado frontend del proyecto, garantizando una interfaz atractiva para el cliente. Implementé un sistema CRUD para la gestión de datos del cliente, creé APIs para facilitar la comunicación entre el frontend y el backend, y seleccioné las librerías clave para el éxito del proyecto.",
-    },
-    {
-      id: 3,
-      date: "Octubre 2023",
-      title: "Carrera Full Stack - CODERHOUSE",
-      description:
-        "Completé la Carrera Full Stack de Coderhouse, adquiriendo una sólida base en el desarrollo Frontend y Backend. Ahora estoy enfocado en perfeccionar las habilidades que me proporcionaron durante la formación.",
-    },
-  ];
+  const { t } = useTranslation();
+  const EXPERIENCE = t("EXPERIENCE", { returnObjects: true });
 
   return (
     <StyledCelExperience id="trayectoria" className="dark:bg-slate-900">
       <div className="w-full mx-auto lg:w-[740px] md:w-[670px] py-40 	">
         <h2 className="text-white text-2xl font-semibold  mb-6 flex gap-x-2 items-center">
-        <Briefcase className="size-8"/> Experiencia
+          <Briefcase className="size-8" /> {t("Experiencia.title")}
         </h2>
         <ol className="relative border-s border-gray-200 dark:border-gray-700">
           {EXPERIENCE.map((experiencie) => (
--- src/components/proyecto/Proyecto.jsx
+++ src/components/proyecto/Proyecto.jsx
@@ -3,73 +3,9 @@ import pro from "../../styles/proyecto.module.css";
 import ProyectoItem from "./ProyectoItem.jsx";
 import { useTranslation } from "react-i18next";
 
-const PROYECTO = [
-  {
-    id: 1,
-    name: "Ecommerce - FireBase",
-    description: "descripcion del proyecto hecho",
-    skills: [
-      "React",
-      "Material-UI",
-      "Axios",
-      "Bootstrap",
-      "Firebase",
-      "Formik",
-      "Yup",
-      "React Router",
-      "JSON Server",
-      "SweetAlert2",
-    ],
-    img: "/proyectos-img/proyecto1.webp",
-    alt: "contexto de la imagen",
-    href: "https://proyect-react-2.vercel.app/",
-    code : "https://github.com/BigNitgh/Proyect-React-2"
-  },
-  {
-    id: 2,
-    name: "Full Stack",
-    description: "descripcion del proyecto hecho",
-    skills: [
-      "Handlebars",
-    "CSS",
-    "JavaScript",
-    "Node.js",
-    "Express",
-    "MongoDB",
-    "Bootstrap",
-    "JWT",
-    "Swagger",
-    "Socket.io",
-    "Mongoose",
-    "Handlebars",
-    "Passport",
-    "bcrypt",
-    "Winston",
-    "Chai",
-    "Mocha",
-    "Supertest"
-    ],
-    img: "/proyectos-img/Proyecto2.webp",
-    alt: "contexto de la imagen",
-    href: "https://probandoloscambios.onrender.com",
-    code : "https://github.com/BigNitgh/Pre-entrega-backend/tree/ImpLog"
-
-  },
-  {
-    id: 3,
-    name: "proyecto3",
-    description: "descripcion del proyecto hecho",
-    skills: ["JavaScript", "HTML", "CSS"],
-    img: "/proyectos-img/Proyecto2.webp",
-    alt: "contexto de la imagen",
-    href: "https://probandoloscambios.onrender.com",
-    code : "https://github.com/BigNitgh/Proyect-React-2"
-
-  },
-];
-
 const Proyecto = () => {
-  const {t} =useTranslation()
+  const { t } = useTranslation();
+  const PROYECTO = t("PROYECTOS", { returnObjects: true });
   return (
     <section id="proyecto" className="bg-white dark:bg-slate-900	  ">
       <div className="mx-auto max-w-screen-xl w-full">
